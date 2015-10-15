3d9d867987f5fd9d1b443370ad5227e311f0cd60
Oleksandr Gura <Oleksandr_Gura@epam.com>
2015-10-15 10:23:57 +0300

    6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static final String URL = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
 
     public static void main(String[] args) throws MalformedURLException, IOException {
-        //for commit12345
+        //for commit123456
         
         File destination = new File(System.getProperty("user.dir")+"\\result.txt");
         System.out.println("Downloading file...");
