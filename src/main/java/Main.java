2c7d7a4ccb5344b1edab594cc731157920bf90b1
Oleksandr Gura <Oleksandr_Gura@epam.com>
2015-10-15 09:03:07 +0300

    File download project

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
+
+public class Main {
+    
+    public static final String URL = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        
+        File destination = new File(System.getProperty("user.dir")+"\\result.txt");
+        System.out.println("Downloading file...");
+        FileUtils.copyURLToFile(new URL(URL), destination);      
+        System.out.println("File is successfully recieved!");
+    }
+
+}
